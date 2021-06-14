7c6926b9aab1cac72dc490ee86db060e89691fb6
Antoine Lamy <antoinelamy@gmail.com>
2021-06-14 16:50:18 -0400

    Properly reference is_ci from enterprise_configuration action (#17)
    * Prefix is_ci with action name
    
    * Fix action namespace
    
    * Take 3

--- fastlane/actions/enterprise_configuration.rb
+++ fastlane/actions/enterprise_configuration.rb
@@ -75,7 +75,7 @@ module Fastlane
       def self.run(params)
         is_jenkins_ci = ENV["EXEC_RUNNING_ON_JENKINS"] != nil && strip_quotes(ENV["EXEC_RUNNING_ON_JENKINS"]) == "YES"
 
-        if is_jenkins_ci || is_ci
+        if is_jenkins_ci || other_action.is_ci
           genericProvisioningProfile = Model::ProvisioningProfile.new(
             path: "#{strip_quotes(ENV["PROVISIONING_DIR"])}/#{strip_quotes(ENV["PROVISIONING_FILE"])}"
           )
